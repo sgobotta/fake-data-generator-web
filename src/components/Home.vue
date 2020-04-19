1dcdd02ef0345d4ee9b4618d6860985f818ea015
sgobotta <santiago@camba.coop>
2020-04-19 12:28:58 -0300

    :computer: adds codemirror as dependency and implements a simple homepage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,113 +1,129 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div class="container">
+    <div class="code-container">
+      <codemirror v-model="code" :options="cmOption" />
+    </div>
+    <div class="pre-container">
+      <pre class="pre">{{ code }}</pre>
+    </div>
   </div>
 </template>
 
 <script>
+import debounce from 'lodash.debounce'
+import { codemirror } from 'vue-codemirror'
+
+// base style
+import 'codemirror/lib/codemirror.css'
+
+// theme css
+import 'codemirror/theme/base16-dark.css'
+// language
+import 'codemirror/mode/vue/vue.js'
+
+// active-line.js
+import 'codemirror/addon/selection/active-line.js'
+
+// styleSelectedText
+import 'codemirror/addon/selection/mark-selection.js'
+import 'codemirror/addon/search/searchcursor.js'
+
+// highlightSelectionMatches
+import 'codemirror/addon/scroll/annotatescrollbar.js'
+import 'codemirror/addon/search/matchesonscrollbar.js'
+import 'codemirror/addon/search/match-highlighter.js'
+
+// keyMap
+import 'codemirror/mode/clike/clike.js'
+import 'codemirror/addon/edit/matchbrackets.js'
+import 'codemirror/addon/comment/comment.js'
+import 'codemirror/addon/dialog/dialog.js'
+import 'codemirror/addon/dialog/dialog.css'
+import 'codemirror/addon/search/search.js'
+import 'codemirror/keymap/sublime.js'
+
+// foldGutter
+import 'codemirror/addon/fold/foldgutter.css'
+import 'codemirror/addon/fold/brace-fold.js'
+import 'codemirror/addon/fold/comment-fold.js'
+import 'codemirror/addon/fold/foldcode.js'
+import 'codemirror/addon/fold/foldgutter.js'
+import 'codemirror/addon/fold/indent-fold.js'
+import 'codemirror/addon/fold/markdown-fold.js'
+import 'codemirror/addon/fold/xml-fold.js'
+
 export default {
-  name: 'HelloWorld',
+  name: 'Home',
+  components: {
+    codemirror
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      code: 'const A = 10',
+      cmOption: {
+        tabSize: 2,
+        styleActiveLine: true,
+        lineNumbers: true,
+        line: true,
+        foldGutter: true,
+        styleSelectedText: true,
+        mode: 'text/javascript',
+        matchBrackets: true,
+        showCursorWhenSelecting: true,
+        theme: 'base16-dark',
+        extraKeys: { 'Ctrl': 'autocomplete' },
+        hintOptions: {
+          completeSingle: false
+        }
+      }
+    }
+  },
+  methods: {
+    delayParse () {
+      if (this.shouldParse) {
+        this.shouldParse = false
+        this.parseModel()
+      }
+    },
+    parseModel (newValue) {
+      console.log('new value ::: ', newValue)
+      console.log('parsing...', this.code)
+      this.parsedModel = `-${this.parsedModel}`
+    }
+  },
+  watch: {
+    code (newValue) {
+      this.shouldParse = true
+      debounce(this.delayParse, 500)()
     }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
+<style lang="scss">
+body {
+  padding-left: 40px;
+  padding-right: 40px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.container {
+  text-align: left;
+  display: flex;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.code-container {
+  flex: 1;
+  width: 100%
 }
-a {
-  color: #42b983;
+.pre-container {
+  flex: 1;
+  max-height: 400px;
+  width: 100%;
+}
+.pre {
+  padding-top: 4px;
+  margin: 0;
+  height: 296px;
+  overflow: scroll;
+  padding-left: 20px;
 }
 </style>
